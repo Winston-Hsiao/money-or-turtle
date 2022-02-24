d199e783bfb77fb80710e7bfa3d86daaa44c6b45
Winston Hsiao <96440583+Winston-Hsiao@users.noreply.github.com>
2022-02-23 19:09:41 -0500

    change turtle transformation display method
    hidden/visible vs changing inner text

--- turtleButtonScript.js
+++ turtleButtonScript.js
@@ -4,6 +4,7 @@ let balanceT = document.getElementById("balance") // balance text
 let countT = document.getElementById("count") // count text
 let balance = 0
 let count = 0
+document.getElementById('turtle').style.visibility='hidden';
 
 function clickButton() {
     balance = balance + 25000 // add $25,000 to balance
@@ -21,7 +22,7 @@ function clickButton() {
     let random_num = Math.floor(Math.random() * 100) + 1; // random num gen
     
     if (random_num == 1) { // 1% chance to turn into a turtle
-        random.innerText = "You turned into a turtle! oh no... or yay?"
+        document.getElementById('turtle').style.visibility='visible';
         document.getElementById('button').style.visibility='hidden';
     }
 }
@@ -30,6 +31,7 @@ function reset() {
     balance = 0
     count = 0
     document.getElementById('button').style.visibility='visible';
+    document.getElementById('turtle').style.visibility='hidden';
     balanceT.innerText = "$0.00"
     countT.innerText = "You've clicked the button 0 times"
     random.innerText = " "
